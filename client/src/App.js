85064728e0518c26fc7e84cc78393016c2fbb543
Liam <liambhetherington@gmail.com>
2022-05-17 18:50:12 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './scss/styles.scss';
+
+import Todo from './components/Todo';
+import LandingPage from './pages/LandingPage';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='landing-page'>
+      <h1>Connecting programmers with clients faster than ever.</h1>
+      <div className='register-cta'>
+        <button>Find a developer</button>
+        <button>Start picking up tickets</button>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
