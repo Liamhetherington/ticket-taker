0d373a5b1dd2a97352ad9bf11897735f0d822523
Liam <liambhetherington@gmail.com>
2022-05-17 20:08:01 -0400

    landing page cta buttons

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import IconButton from '../components/IconButton'
+
+import { faTicket, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const LandingPage = () => {
   return (
     <div className='landing-page'>
       <h1>Connecting programmers with clients faster than ever.</h1>
       <div className='register-cta'>
-        <button>Find a developer</button>
-        <button>Start picking up tickets</button>
+        <IconButton
+          icon={faMagnifyingGlass}
+          text={'Find a developer now'}
+          onClick={() => {
+            console.log('redirect to company registration')
+          }}
+        />
+        <div className='divider'></div>
+        <IconButton
+          icon={faTicket}
+          text={'Start taking tickets today'}
+          onClick={() => {
+            console.log('redirect to developer registration')
+          }}
+        />
       </div>
     </div>
   )
